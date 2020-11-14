fa72cd8ee49402e7120f524b042fed5768220696
luisfernandomp <53625024+luisfernandomp@users.noreply.github.com>
2020-11-14 00:48:03 -0300

    Projeto Nyous Mobile iniciado

--- App.js
+++ App.js
@@ -1,13 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+//Páginas
+import Login from './pages/Login';
+import Home from './pages/Home';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+//Navigations
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+ 
+const Autenticado = () => {
+  return(
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Logout" component={Logout} />
+      </Drawer.Navigator>
+  )
+}
+
+const Logout = () => {
+  return(
+    <View>
+      <Text>Deseja realmente sair da aplicação?</Text>
+      <Button onPress={() => {
+          AsyncStorage.removeItem('@jwt');
+          navigation.push('Login');
+      } } title='Sair'></Button>
+    </View>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown : false}}>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Autenticado" component={Autenticado} />
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
