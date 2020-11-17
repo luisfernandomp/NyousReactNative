c945090a5fd3cc50d6a18736179212f54ce4c366
luisfernandomp <53625024+luisfernandomp@users.noreply.github.com>
2020-11-17 11:43:47 -0300

    Listando eventos

--- App.js
+++ App.js
@@ -1,49 +1,49 @@
+  
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
-//Páginas
-import Login from './pages/Login';
-import Home from './pages/Home';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-//Navigations
+// Navigations
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import { NavigationContainer } from '@react-navigation/native';
 
 const Drawer = createDrawerNavigator();
-const Stack = createStackNavigator();
- 
+const Stack  = createStackNavigator();
+
+// Paginas
+import Login from './pages/Login'
+import Home  from './pages/Home'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 const Autenticado = () => {
   return(
-      <Drawer.Navigator initialRouteName="Home">
+    <Drawer.Navigator initialRouteName="Home">
         <Drawer.Screen name="Home" component={Home} />
         <Drawer.Screen name="Logout" component={Logout} />
-      </Drawer.Navigator>
+    </Drawer.Navigator>
   )
 }
 
-const Logout = () => {
+const Logout = ( {navigation} ) => {
   return(
     <View>
       <Text>Deseja realmente sair da aplicação?</Text>
       <Button onPress={() => {
-          AsyncStorage.removeItem('@jwt');
-          navigation.push('Login');
-      } } title='Sair'></Button>
+        AsyncStorage.removeItem('@jwt');
+        navigation.push('Login');
+      }} title="SAIR" ></Button>
     </View>
   )
 }
 
 export default function App() {
   return (
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown : false}}>
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Autenticado" component={Autenticado} />
-        </Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Autenticado">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Autenticado" component={Autenticado} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
